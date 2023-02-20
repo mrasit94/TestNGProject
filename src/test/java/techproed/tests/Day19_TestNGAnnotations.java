04444751c41403eec3c2bd3048f58d7ba6a9c21d
mrasit94 <rsdmuhammet@gmail.com>
2023-02-19 23:19:17 -0500

    19/02/2023

--- src/test/java/techproed/tests/Day19_TestNGAnnotations.java
+++ src/test/java/techproed/tests/Day19_TestNGAnnotations.java
@@ -59,7 +59,7 @@ public class Day19_TestNGAnnotations {
     public void afterMethod(){
         System.out.println("After Method");
     }
-    @Test(priority = 1)
+    @Test(priority = 1, groups = "regression-tests")
     public void test7(){
         System.out.println("Test 7");
     }
@@ -67,7 +67,7 @@ public class Day19_TestNGAnnotations {
     public void test2(){
         System.out.println("Test 2");
     }
-    @Test(enabled = false)
+    @Test(enabled = false, groups = "regression-tests")
     public void test3(){
         System.out.println("Test 3");
     }
